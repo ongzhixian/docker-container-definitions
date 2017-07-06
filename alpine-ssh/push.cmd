ac556ec6717568e3fc969ca0bf2380fc4b51d341
zhixian <zhixian@hotmail.com>
2017-07-07 00:31:34 +0800

    Update build and push scripts to build and push to docker hub

--- alpine-ssh/push.cmd
+++ alpine-ssh/push.cmd
@@ -4,4 +4,4 @@ REM ############################################################################
 REM Script to build docker container
 REM ################################################################################
 
-docker build -t zhixian/alpine-ssh .
+docker push zhixian/alpine-ssh
